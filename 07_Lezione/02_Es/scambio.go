821b858c19b04639323135a60cca48c0ef5d2910
viac92 <64146594+viac92@users.noreply.github.com>
2020-11-19 15:59:49 +0100

    aggiunto esercizio esemplificativo

---
+++ 07_Lezione/02_Es/scambio.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func scambioV1(str1, str2 string) { //versione 1
+	str1, str2 = str2, str1
+}
+
+func scambioV2(str1, str2 *string) { //versione 2
+	*str1, *str2 = *str2, *str1
+}
+
+func main() {
+	s1 := "ciao"
+	s2 := "hello"
+
+	fmt.Println(s1, s2)
+
+	scambioV1(s1, s2)                        //versione 1
+	fmt.Println("scambio V1:", s1, s2) 
+
+	scambioV2(&s1, &s2)                  //versione 2
+	fmt.Println("scambio V2:", s1, s2)
+
+}
