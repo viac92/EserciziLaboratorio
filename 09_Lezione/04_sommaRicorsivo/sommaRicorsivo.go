96a1be2aef16786dcf5ed8bac2b7109e855c0d75
viac92 <64146594+viac92@users.noreply.github.com>
2020-12-03 20:22:01 +0100

    risolto esercizio

---
+++ 09_Lezione/04_sommaRicorsivo/sommaRicorsivo.go
@@ -0,0 +1,22 @@
+// Scrivere una funzione ricorsiva f(n) che restituisce la somma di una lista di interi
+package main
+
+import (
+	"fmt"
+)
+
+func f(n []int) int {
+	if len(n) == 1 {
+		return n[0]
+	}
+	return n[0] + f(n[1:]) 
+}
+
+
+func main() {
+	//lista := []int{1}
+	//lista := []int{1, 2}
+	lista := []int{4, 5 , 6, 5, 10, 20}
+	fmt.Println(f(lista))
+}
+
