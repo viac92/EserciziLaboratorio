abfa75dfeb2233f6997e8cd01dcb2d70395b4d47
viac92 <64146594+viac92@users.noreply.github.com>
2021-01-16 14:55:57 +0100

    started

---
+++ 11_ProvaEsame/TemaProvaEsame/cerchi/cerchi.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+
+)
+
+type Cerchio struct {
+	nome string
+	x, y, raggio float64
+}
+
+// una funzione 'newCircle(descr string) Cerchio'
+// che data una stringa che descrive un cerchio
+// (nome, coordinate x e y del centro, raggio separati da spazi)
+// restituisce il cerchio corrispondente
+func newCircle(descr string) Cerchio {
+	
+}
